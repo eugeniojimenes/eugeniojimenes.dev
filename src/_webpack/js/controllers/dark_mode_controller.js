eb8859507664974693c6cfebb6f5fb7cb3185a24
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-07-21 19:12:39 -0300

    feat(dark-mode-icon): add toggle icons
      - Add toggle icons for light mode and dark mode
      - Improve DarkModeController
      - Improve some shadows box for dark mode

--- src/_webpack/js/controllers/dark_mode_controller.js
+++ src/_webpack/js/controllers/dark_mode_controller.js
@@ -1,32 +1,41 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "darkIcon", "lightIcon" ]
 
   connect() {
     console.log(`Hello, I'm the Stimulus DarkModeController`)
-  }
-
-  useDarkMode() {
-    if(localStorage.theme === 'light') {
-      document.documentElement.classList.add('dark')
-      // localStorage.setItem('color-theme', 'dark')
-      localStorage.theme = 'dark'
+    // Change the icons inside the button based on previous settings
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      this.lightIconTarget.classList.remove('hidden');
+    } else {
+      this.darkIconTarget.classList.remove('hidden');
     }
   }
 
-  useLightMode() {
-    if(localStorage.theme === 'dark') {
-      document.documentElement.classList.remove('dark')
-      // localStorage.setItem('color-theme', 'light')
-      localStorage.theme = 'light'
+  toggle() {
+    // toggle icons inside button
+    this.lightIconTarget.classList.toggle('hidden');
+    this.darkIconTarget.classList.toggle('hidden');
+    // if set via local storage previously
+    if (localStorage.theme) {
+        if (localStorage.theme === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.theme = 'dark'
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = 'light'
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = 'light'
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.theme = 'dark'
+        }
     }
   }
-
-  // checkTheme() {
-  //   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  //     document.documentElement.classList.add('dark')
-  //   } else {
-  //     document.documentElement.classList.remove('dark')
-  //   }
-  // }
 }
